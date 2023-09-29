416efb4775fd54acc3888df58f2903fc30d3dcdb
Jose Manuel Castro Edreira <itcjosemanuelcastro@gmail.com>
2023-09-29 02:37:33 +0200

    ProductController Terminado

--- src/main/java/com/campusdual/appmazing/controller/ProductController.java
+++ src/main/java/com/campusdual/appmazing/controller/ProductController.java
@@ -4,26 +4,52 @@ import com.campusdual.appmazing.api.iProductService;
 import com.campusdual.appmazing.model.dto.ProductDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
     @Autowired
     iProductService productService;
+
     @GetMapping
-    public String testController(){
+    public String testController() {
         return "Product controller works!";
     }
+
     @PostMapping
-    public String testControllerBody(@RequestBody String name){
+    public String testControllerBody(@RequestBody String name) {
         return "Product controller works, " + name + "!";
     }
+
     @GetMapping(value = "/testMethod")
     public String testControllerMethod() {
         return "Product controller method works!";
     }
-    @PostMapping (value="/get")
-    public ProductDTO queryProduct(@RequestBody ProductDTO product){
+
+    @PostMapping(value = "/get")
+    public ProductDTO queryProduct(@RequestBody ProductDTO product) {
         return this.productService.queryProduct(product);
     }
 
+    @GetMapping(value = "/getAll")
+    public List<ProductDTO> queryAllProducts() {
+        return this.productService.queryAllProducts();
+    }
+
+    @PostMapping(value = "/add")
+    public int insertProduct(@RequestBody ProductDTO product) {
+        return this.productService.insertProduct(product);
+    }
+
+    @PutMapping(value = "/update")
+    public int updateProduct(@RequestBody ProductDTO product) {
+        return this.productService.updateProduct(product);
+    }
+
+    @DeleteMapping(value = "/delete")
+    public int deleteProduct(@RequestBody ProductDTO product) {
+        return this.productService.deleteProduct(product);
+    }
 }
